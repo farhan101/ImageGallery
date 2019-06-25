75827a44a3fd55318fcb97b6682a7fc45b9e7e46
Farhan Arshad <you@example.com>
2019-06-25 17:59:53 +0500

    Indexed UIButton

---
+++ ImageGallery/gallery/IndexedButton.swift
@@ -0,0 +1,24 @@
+//
+//  HGButton.swift
+//  HobbyGlobe2018
+//
+//  Created by Farhan Arshad on 5/3/18.
+//  Copyright © 2018 IntelioSystems. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IndexedButton: UIButton {
+    
+    var index: Int?
+    var info: String?
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+}
