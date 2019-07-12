a57f4691d04be6e48e38fca366979263633762c2
Farhan Arshad <you@example.com>
2019-07-12 21:14:47 +0500

    Useful comments added.

--- ImageGallery/ViewController.swift
+++ ImageGallery/ViewController.swift
@@ -37,17 +37,21 @@ class ViewController: UIViewController, ImageGalleryDelegate {
                                  
     func imageGallery(completion: @escaping ([MediaRecord]?) -> Void) {
         var collection:[MediaRecord] = []
+        //1. Pass Image as url string
         var data = ImageRecord<String>()
         data.media = images[0]
         collection.append(data)
+        //2. Pass Image as UIImage object
         var data1_1 = ImageRecord<UIImage>()
         data1_1.media = UIImage(named: "k2")
         collection.append(data1_1)
+        //3. Pass video thumbnail as url string
         var data2 = VideoRecord<String, String>()
         data2.media = "https://www.radiantmediaplayer.com/media/bbb-360p.mp4"
         data2.thumbnail = images[1]
         collection.append(data2)
         completion(collection)
+        //3. Pass video thumbnail as UIImage object
         var data2_1 = VideoRecord<String, UIImage>()
         data2_1.media = "https://www.radiantmediaplayer.com/media/bbb-360p.mp4"
         data2_1.thumbnail = UIImage(named: "bunny")
